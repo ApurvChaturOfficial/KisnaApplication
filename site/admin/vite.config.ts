144a526b31e22a6849dbef7fdce8cc145125ec14
ApurvChaturOfficial <apurvchaturofficial@gmail.com>
2024-10-27 19:32:09 +0530

    PWA Setup

--- site/admin/vite.config.ts
+++ site/admin/vite.config.ts
@@ -1,10 +1,52 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
+
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: "prompt",
+  includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+  manifest: {
+    name: "Kisna",
+    short_name: "Kisna",
+    description: "This a description for this app",
+    icons: [
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "apple touch icon",
+      },
+      // {
+      //   src: "/maskable_icon.png",
+      //   sizes: "225x225",
+      //   type: "image/png",
+      //   purpose: "any maskable",
+      // },
+    ],
+    theme_color: "#171717",
+    background_color: "#e8ebf2",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugin)],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
